c7f5c44f02fa0f819d995d01644f59b48e774baa
fersus <deriabin_85@mail.ru>
2023-05-16 15:19:04 +0300

    add blog CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from blog import views
 
 urlpatterns = [
     path("blog/", views.BlogListView.as_view(), name="blog_home"),
-    path("post/<int:pk>/", views.BlogDetailView.as_view(), name="post_detail")
+    path("post/<int:pk>/", views.BlogDetailView.as_view(), name="post_detail"),
+    path("post/new/", views.BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit/", views.BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", views.BlogDeleteView.as_view(), name="post_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
 from .models import Post
 
 
@@ -8,6 +8,24 @@ class BlogListView(ListView):
     template_name = 'blog/blog_home.html'
 
 
-class BlogDetailView(DeleteView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('blog_home')
